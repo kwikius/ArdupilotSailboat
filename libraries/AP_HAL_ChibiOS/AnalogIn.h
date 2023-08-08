bugobliterator/ardupilot

AP_HAL_ChibiOS: add support for using ADC2 and ADC3 on H7

libraries/AP_HAL_ChibiOS/AnalogIn.h
@@ -19,7 +19,22 @@
 
 #include "AP_HAL_ChibiOS.h"
 
+// available ADC channels for allocation
 #define ANALOG_MAX_CHANNELS 16
+
+// physical ADC channels per ADC HAL driver
+// This is MCU dependent, currently STM32H7 has the highest number of ADC_INs i.e. 20
+#define HAL_MAX_ANALOG_IN_CHANNELS 20
+
+#ifndef HAL_NUM_ANALOG_INPUTS
+#if defined(HAL_ANALOG3_PINS) || HAL_WITH_MCU_MONITORING
+#define HAL_NUM_ANALOG_INPUTS 3
+#elif defined(HAL_ANALOG2_PINS)
+#define HAL_NUM_ANALOG_INPUTS 2
+#else
+#define HAL_NUM_ANALOG_INPUTS 1
+#endif
+#endif
 
 // number of samples on each channel to gather on each DMA callback
 #define ADC_DMA_BUF_DEPTH 8
@@ -71,8 +86,9 @@ public:
 #endif
 
 private:
-    void read_adc(uint32_t *val);
+    void read_adc(uint8_t index, uint32_t *val);
     void update_power_flags(void);
+    void setup_adc(uint8_t index);
     static void adccallback(ADCDriver *adcp);
 
     ChibiOS::AnalogSource* _channels[ANALOG_MAX_CHANNELS];
@@ -84,31 +100,39 @@ private:
     uint16_t _power_flags;
     uint16_t _accumulated_power_flags;  // bitmask of all _power_flags ever set
 
-    ADCConversionGroup adcgrpcfg;
+    ADCConversionGroup adcgrpcfg[HAL_NUM_ANALOG_INPUTS];
+
+    static uint8_t get_num_grp_channels(uint8_t index);
+    static uint8_t get_pin_channel(uint8_t index, uint8_t pin);
+    static uint8_t get_pin_channel_with_offset(uint8_t index, uint8_t pin);
+    static float get_pin_scaling(uint8_t index, uint8_t pin);
+    static uint8_t get_adc_index(ADCDriver* adcp);
 
     struct pin_info {
         uint8_t channel;
         float scaling;
     };
     static const pin_info pin_config[];
+#ifdef HAL_ANALOG2_PINS
+    static const pin_info pin_config_2[];
+#endif
+#if defined(HAL_ANALOG3_PINS) || HAL_WITH_MCU_MONITORING
+    static const pin_info pin_config_3[];
+#endif
 
-    static adcsample_t *samples;
-    static uint32_t sample_sum[];
-    static uint32_t sample_count;
+    static adcsample_t *samples[HAL_NUM_ANALOG_INPUTS];
+    static uint32_t *sample_sum[HAL_NUM_ANALOG_INPUTS];
+    static uint32_t sample_count[HAL_NUM_ANALOG_INPUTS];
 
     HAL_Semaphore _semaphore;
 
 #if HAL_WITH_MCU_MONITORING
-    // use ADC3 for MCU temperature and voltage monitoring
-    void setup_adc3();
-    void read_adc3(uint32_t *val, uint16_t *min, uint16_t *max);
-    ADCConversionGroup adc3grpcfg;
-    static void adc3callback(ADCDriver *adcp);
-    static adcsample_t *samples_adc3;
-    static uint32_t sample_adc3_sum[];
-    static uint16_t sample_adc3_max[];
-    static uint16_t sample_adc3_min[];
-    static uint32_t sample_adc3_count;
+    uint16_t _mcu_monitor_sample_count;
+    uint32_t _mcu_monitor_temperature_accum;
+    uint32_t _mcu_monitor_voltage_accum;
+    uint16_t _mcu_vrefint_min;
+    uint16_t _mcu_vrefint_max;
+
     float _mcu_temperature;
     float _mcu_voltage;
     float _mcu_voltage_min;

kwikius/ArdupilotSailboat

libraries/AP_HAL_ChibiOS/AnalogIn.h
@@ -19,7 +19,22 @@
 
 #include "AP_HAL_ChibiOS.h"
 
+// available ADC channels for allocation
 #define ANALOG_MAX_CHANNELS 16
+
+// physical ADC channels per ADC HAL driver
+// This is MCU dependent, currently STM32H7 has the highest number of ADC_INs i.e. 20
+#define HAL_MAX_ANALOG_IN_CHANNELS 20
+
+#ifndef HAL_NUM_ANALOG_INPUTS
+#if defined(HAL_ANALOG3_PINS) || HAL_WITH_MCU_MONITORING
+#define HAL_NUM_ANALOG_INPUTS 3
+#elif defined(HAL_ANALOG2_PINS)
+#define HAL_NUM_ANALOG_INPUTS 2
+#else
+#define HAL_NUM_ANALOG_INPUTS 1
+#endif
+#endif
 
 // number of samples on each channel to gather on each DMA callback
 #define ADC_DMA_BUF_DEPTH 8
@@ -71,8 +86,9 @@ public:
 #endif
 
 private:
-    void read_adc(uint32_t *val);
+    void read_adc(uint8_t index, uint32_t *val);
     void update_power_flags(void);
+    void setup_adc(uint8_t index);
     static void adccallback(ADCDriver *adcp);
 
     ChibiOS::AnalogSource* _channels[ANALOG_MAX_CHANNELS];
@@ -84,31 +100,40 @@ private:
     uint16_t _power_flags;
     uint16_t _accumulated_power_flags;  // bitmask of all _power_flags ever set
 
-    ADCConversionGroup adcgrpcfg;
+    ADCConversionGroup adcgrpcfg[HAL_NUM_ANALOG_INPUTS];
+
+    static uint8_t get_num_grp_channels(uint8_t index);
+    static uint8_t get_pin_channel(uint8_t adc_index, uint8_t pin_index);
+    static uint8_t get_analog_pin(uint8_t adc_index, uint8_t pin_index);
+    static float get_pin_scaling(uint8_t adc_index, uint8_t pin_index);
+    static uint8_t get_adc_index(ADCDriver* adcp);
 
     struct pin_info {
         uint8_t channel;
+        uint8_t analog_pin;
         float scaling;
     };
     static const pin_info pin_config[];
+#ifdef HAL_ANALOG2_PINS
+    static const pin_info pin_config_2[];
+#endif
+#if defined(HAL_ANALOG3_PINS) || HAL_WITH_MCU_MONITORING
+    static const pin_info pin_config_3[];
+#endif
 
-    static adcsample_t *samples;
-    static uint32_t sample_sum[];
-    static uint32_t sample_count;
+    static adcsample_t *samples[HAL_NUM_ANALOG_INPUTS];
+    static uint32_t *sample_sum[HAL_NUM_ANALOG_INPUTS];
+    static uint32_t sample_count[HAL_NUM_ANALOG_INPUTS];
 
     HAL_Semaphore _semaphore;
 
 #if HAL_WITH_MCU_MONITORING
-    // use ADC3 for MCU temperature and voltage monitoring
-    void setup_adc3();
-    void read_adc3(uint32_t *val, uint16_t *min, uint16_t *max);
-    ADCConversionGroup adc3grpcfg;
-    static void adc3callback(ADCDriver *adcp);
-    static adcsample_t *samples_adc3;
-    static uint32_t sample_adc3_sum[];
-    static uint16_t sample_adc3_max[];
-    static uint16_t sample_adc3_min[];
-    static uint32_t sample_adc3_count;
+    uint16_t _mcu_monitor_sample_count;
+    uint32_t _mcu_monitor_temperature_accum;
+    uint32_t _mcu_monitor_voltage_accum;
+    uint16_t _mcu_vrefint_min;
+    uint16_t _mcu_vrefint_max;
+
     float _mcu_temperature;
     float _mcu_voltage;
     float _mcu_voltage_min;