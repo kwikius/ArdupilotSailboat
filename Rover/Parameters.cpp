peterbarker/ardupilot

Rover: add DOCK mode in rover
This mode will maneuver the rover towards a docking target automatically

Rover/Parameters.cpp
@@ -670,6 +670,12 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("MANUAL_OPTIONS", 53, ParametersG2, manual_options, 0),
 
+    #if PRECISION_LANDING == ENABLED
+    // @Group: DOCK
+    // @Path: mode_dock.cpp
+    AP_SUBGROUPPTR(mode_dock_ptr, "DOCK", 54, ParametersG2, ModeDock),
+    #endif
+
     AP_GROUPEND
 };
 
@@ -715,6 +721,9 @@ ParametersG2::ParametersG2(void)
     smart_rtl(),
 #if HAL_PROXIMITY_ENABLED
     proximity(),
+#endif
+#if PRECISION_LANDING == ENABLED
+    mode_dock_ptr(&rover.mode_dock),
 #endif
     avoid(),
     follow(),

kwikius/ArdupilotSailboat

Rover/Parameters.cpp
@@ -670,6 +670,12 @@ const AP_Param::GroupInfo ParametersG2::var_info[] = {
     // @User: Advanced
     AP_GROUPINFO("MANUAL_OPTIONS", 53, ParametersG2, manual_options, 0),
 
+#if MODE_DOCK_ENABLED == ENABLED
+    // @Group: DOCK
+    // @Path: mode_dock.cpp
+    AP_SUBGROUPPTR(mode_dock_ptr, "DOCK", 54, ParametersG2, ModeDock),
+#endif
+
     AP_GROUPEND
 };
 
@@ -713,6 +719,9 @@ ParametersG2::ParametersG2(void)
     wheel_rate_control(wheel_encoder),
     attitude_control(),
     smart_rtl(),
+#if MODE_DOCK_ENABLED == ENABLED
+    mode_dock_ptr(&rover.mode_dock),
+#endif
 #if HAL_PROXIMITY_ENABLED
     proximity(),
 #endif