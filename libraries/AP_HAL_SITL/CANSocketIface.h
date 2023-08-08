bugobliterator/ardupilot

AP_HAL_SITL: change to 16 bit timeout in event interface
prevent issues on 16 bit timers where we can end up with TIME_INFINITE

libraries/AP_HAL_SITL/CANSocketIface.h
@@ -132,7 +132,7 @@ public:
     public:
         // we just poll fd, no signaling is done
         void signal(uint32_t evt_mask) override { return; }
-        bool wait(uint64_t duration, AP_HAL::EventHandle* evt_handle) override;
+        bool wait(uint16_t duration, AP_HAL::EventHandle* evt_handle) override;
     };
 
 private:

kwikius/ArdupilotSailboat

libraries/AP_HAL_SITL/CANSocketIface.h
@@ -132,7 +132,7 @@ public:
     public:
         // we just poll fd, no signaling is done
         void signal(uint32_t evt_mask) override { return; }
-        bool wait(uint64_t duration, AP_HAL::EventHandle* evt_handle) override;
+        bool wait(uint16_t duration_us, AP_HAL::EventHandle* evt_handle) override;
     };
 
 private: