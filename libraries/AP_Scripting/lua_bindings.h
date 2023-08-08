bugobliterator/ardupilot

AP_Scripting: add mavlink support to scripting

libraries/AP_Scripting/lua_bindings.h
@@ -14,3 +14,6 @@ int lua_dirlist(lua_State *L);
 int lua_removefile(lua_State *L);
 int SRV_Channels_get_safety_state(lua_State *L);
 int lua_get_PWMSource(lua_State *L);
+int lua_mavlink_receive(lua_State *L);
+int lua_mavlink_register_msgid(lua_State *L);
+int lua_mavlink_send(lua_State *L);

kwikius/ArdupilotSailboat

libraries/AP_Scripting/lua_bindings.h
@@ -14,3 +14,7 @@ int lua_dirlist(lua_State *L);
 int lua_removefile(lua_State *L);
 int SRV_Channels_get_safety_state(lua_State *L);
 int lua_get_PWMSource(lua_State *L);
+int lua_mavlink_init(lua_State *L);
+int lua_mavlink_receive(lua_State *L);
+int lua_mavlink_receive_msgid(lua_State *L);
+int lua_mavlink_send(lua_State *L);