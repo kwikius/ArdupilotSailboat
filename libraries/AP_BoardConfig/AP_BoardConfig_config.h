vgade-invn/ardupilot

AP_BoardConfig: add BRD_SD_MISSION parameter

libraries/AP_BoardConfig/AP_BoardConfig_config.h
@@ -1,0 +1,37 @@
+#pragma once
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Filesystem/AP_Filesystem_config.h>
+
+#ifndef AP_FEATURE_BOARD_DETECT
+#if defined(HAL_CHIBIOS_ARCH_FMUV3) || defined(HAL_CHIBIOS_ARCH_FMUV4) || defined(HAL_CHIBIOS_ARCH_FMUV5) || defined(HAL_CHIBIOS_ARCH_MINDPXV2) || defined(HAL_CHIBIOS_ARCH_FMUV4PRO) || defined(HAL_CHIBIOS_ARCH_BRAINV51) || defined(HAL_CHIBIOS_ARCH_BRAINV52) || defined(HAL_CHIBIOS_ARCH_UBRAINV51) || defined(HAL_CHIBIOS_ARCH_COREV10) || defined(HAL_CHIBIOS_ARCH_BRAINV54)
+#define AP_FEATURE_BOARD_DETECT 1
+#else
+#define AP_FEATURE_BOARD_DETECT 0
+#endif
+#endif
+
+#ifndef AP_FEATURE_RTSCTS
+#define AP_FEATURE_RTSCTS 0
+#endif
+
+#ifndef AP_FEATURE_SBUS_OUT
+#define AP_FEATURE_SBUS_OUT 0
+#endif
+
+#ifndef HAL_WATCHDOG_ENABLED_DEFAULT
+#define HAL_WATCHDOG_ENABLED_DEFAULT false
+#endif
+
+#if HAL_HAVE_IMU_HEATER
+#ifndef HAL_IMUHEAT_P_DEFAULT
+#define HAL_IMUHEAT_P_DEFAULT 200
+#endif
+#ifndef HAL_IMUHEAT_I_DEFAULT
+#define HAL_IMUHEAT_I_DEFAULT 0.3
+#endif
+#endif
+
+#ifndef AP_SDCARD_STORAGE_ENABLE
+#define AP_SDCARD_STORAGE_ENABLE (HAL_MEM_CLASS >= HAL_MEM_CLASS_1000) && HAVE_FILESYSTEM_SUPPORT && BOARD_FLASH_SIZE > 1024
+#endif

kwikius/ArdupilotSailboat

libraries/AP_BoardConfig/AP_BoardConfig_config.h
@@ -1,0 +1,37 @@
+#pragma once
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Filesystem/AP_Filesystem_config.h>
+
+#ifndef AP_FEATURE_BOARD_DETECT
+#if defined(HAL_CHIBIOS_ARCH_FMUV3) || defined(HAL_CHIBIOS_ARCH_FMUV4) || defined(HAL_CHIBIOS_ARCH_FMUV5) || defined(HAL_CHIBIOS_ARCH_MINDPXV2) || defined(HAL_CHIBIOS_ARCH_FMUV4PRO) || defined(HAL_CHIBIOS_ARCH_BRAINV51) || defined(HAL_CHIBIOS_ARCH_BRAINV52) || defined(HAL_CHIBIOS_ARCH_UBRAINV51) || defined(HAL_CHIBIOS_ARCH_COREV10) || defined(HAL_CHIBIOS_ARCH_BRAINV54)
+#define AP_FEATURE_BOARD_DETECT 1
+#else
+#define AP_FEATURE_BOARD_DETECT 0
+#endif
+#endif
+
+#ifndef AP_FEATURE_RTSCTS
+#define AP_FEATURE_RTSCTS 0
+#endif
+
+#ifndef AP_FEATURE_SBUS_OUT
+#define AP_FEATURE_SBUS_OUT 0
+#endif
+
+#ifndef HAL_WATCHDOG_ENABLED_DEFAULT
+#define HAL_WATCHDOG_ENABLED_DEFAULT false
+#endif
+
+#if HAL_HAVE_IMU_HEATER
+#ifndef HAL_IMUHEAT_P_DEFAULT
+#define HAL_IMUHEAT_P_DEFAULT 200
+#endif
+#ifndef HAL_IMUHEAT_I_DEFAULT
+#define HAL_IMUHEAT_I_DEFAULT 0.3
+#endif
+#endif
+
+#ifndef AP_SDCARD_STORAGE_ENABLED
+#define AP_SDCARD_STORAGE_ENABLED (HAL_MEM_CLASS >= HAL_MEM_CLASS_1000) && HAVE_FILESYSTEM_SUPPORT && BOARD_FLASH_SIZE > 1024
+#endif